52514b8f1c9f1468a558d2424605e03ddf092ea8
felixcch <32195330+felixcch@users.noreply.github.com>
2022-05-01 11:57:43 +0800

    -init commit Kafka project

---
+++ src/main/java/com/tech/kafka/model/User.java
@@ -0,0 +1,14 @@
+package com.tech.kafka.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private String dept;
+    private Long salary;
+}
